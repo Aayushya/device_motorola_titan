xc-racer99/android_device_motorola_falcon

falcon: Initial Omni bringup
Bring back custom mkbootimg

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_falcon.mk
+    $(LOCAL_DIR)/omni_falcon.mk

Aayushya/device_motorola_titan

Conflicts:
	AndroidProducts.mk
	BoardConfig.mk
	cm.mk
	full_falcon.mk
	full_titan.mk
	omni_falcon.mk

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_titan.mk
+    $(LOCAL_DIR)/omni_titan.mk